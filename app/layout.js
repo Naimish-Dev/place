41d45f88d459eeda0f928a9629981a98e41290f2
Naimish Bhesaniya <102242463+Naimish-Dev@users.noreply.github.com>
2024-05-11 14:17:49 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Place",
+  title: "Palace residence Dubai Hills estate",
   description: "Palace Residences Dhe Payment Plan",
 };
 
